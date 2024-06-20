90eaa05e58d6b9f29492052b9d9d550df266cf38
PintykAndrey <pintykandrey@gmail.com>
2024-06-20 17:32:59 +0300

    first commit

---
+++ 1ch/1ch/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1ch
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string serverIp = "127.0.0.1";
+            int port = 8888;
+            TcpClient client = new TcpClient();
+
+            try
+            {
+                await client.ConnectAsync(serverIp, port);
+                Console.WriteLine("Connected to server.");
+                NetworkStream stream = client.GetStream();
+                Console.Write("Enter your login: ");
+                string login = Console.ReadLine();
+                string loginMessage = $"LOGIN:{login}";
+                byte[] loginData = Encoding.UTF8.GetBytes(loginMessage);
+                await stream.WriteAsync(loginData, 0, loginData.Length);
+                Console.WriteLine("Login sent to server.");
+                Task readingTask = Task.Run(async () =>
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    try
+                    {
+                        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                        {
+                            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);a
+                            Console.Write("\r" + new string(' ', Console.WindowWidth - 1) + "\r");
+                            Console.Write(message);
+                            Console.Write("\nEnter message: ");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                });
+                while (true)
+                {
+                    Console.Write("Enter message: ");
+                    string message = Console.ReadLine();
+                    byte[] messageData = Encoding.UTF8.GetBytes(message);
+                    await stream.WriteAsync(messageData, 0, messageData.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+        private static List<string> messages = new List<string>();
+
+        static async Task Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+
+            TcpListener listener = new TcpListener(ipAddress, port);
+            listener.Start();
+            Console.WriteLine("The server is running. Waiting for connections...");
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Client conected.");
+
+                _ = HandleClient(client);
+            }
+        }
+
+        static async Task HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            string clientTag = Guid.NewGuid().ToString();
+
+            try
+            {
+                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                {
+                    string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Received: " + dataReceived);
+
+                    if (dataReceived.StartsWith("LOGIN:"))
+                    {
+                        string login = dataReceived.Substring(6);
+                        string clientKey = $"{clientTag}:{login}";
+                        clients.Add(clientKey, client);
+                        Console.WriteLine($"Client '{login}' add with tag '{clientTag}'.");
+                    }
+                    else
+                    {
+                        string senderClientKey = clients.FirstOrDefault(x => x.Value == client).Key;
+                        string senderLogin = senderClientKey.Split(':')[1];
+                        string messageWithSender = $"{senderLogin}: {dataReceived}";
+                        messages.Add(messageWithSender);
+                        BroadcastMessage(messageWithSender, senderClientKey);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+
+        static void BroadcastMessage(string message, string senderClientKey)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            foreach (var entry in clients)
+            {
+                if (entry.Key != senderClientKey)
+                {
+                    TcpClient targetClient = entry.Value;
+                    SendMessageToClient(targetClient, message);
+                }
+            }
+        }
+
+        static async void SendMessageToClient(TcpClient client, string message)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(messageBytes, 0, messageBytes.Length);
+        }
+    }
+}
